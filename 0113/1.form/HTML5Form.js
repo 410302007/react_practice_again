1fc3b83c8796936a7e3350bdd6f9c21bf01ee0bd
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-19 16:50:58 +0800

    可控表單元件- textarea

--- 0113/1.form/HTML5Form.js
+++ 0113/1.form/HTML5Form.js
@@ -3,6 +3,9 @@ import {useState} from 'react'
 function HTML5Form(){
   //input-text
   const [inputText, setInputText]= useState('')
+
+  //textarea
+  const [textareaText, setTextareaText] = useState('');
   return (
   <>
   <h1>可控的表單元件範例</h1>
@@ -12,6 +15,15 @@ function HTML5Form(){
       setInputText(e.target.value)
     }} />
     </section>
+    <section id="textarea">
+        <h2>文字輸入區域(textarea)</h2>
+        <textarea
+          value={textareaText}
+          onChange={(e) => {
+            setTextareaText(e.target.value);
+          }}
+        />
+      </section>
   </>
   )
 }
