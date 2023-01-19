588440595ed553520a1162a5a81f953dcc0fb640
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-19 17:48:34 +0800

    可控表單元間- checkgourp

--- 0113/1.form/HTML5Form.js
+++ 0113/1.form/HTML5Form.js
@@ -15,7 +15,12 @@ function HTML5Form(){
   //checkbox_one
   //一開始預設為不同意
   const [agree, setAgree] = useState(false);
-  
+
+  //checkbox group - 第一種方法
+  //state中藥記錄使用者選了哪幾個值(複選)
+  const [likeList, setLikeList] = useState(['芒果']); //預設值為芒果
+  const fruitOptions = ['芒果', '香蕉'];
+
   return (
   <>
   <h1>可控的表單元件範例</h1>
@@ -54,7 +59,7 @@ function HTML5Form(){
         })}
       </section>
       <section id="checkbox-one">
-        <h2>核取方塊(單一)</h2>
+        <h2>核取方塊(單一)(checkbox)</h2>
         <input
           type="checkbox"
           checked={agree}
@@ -63,6 +68,37 @@ function HTML5Form(){
           }}
         />
         <label>我已閱讀並同意....</label>
+      </section>
+      <section id="checkbox-group">
+        <h2>核取方塊(群組)(checkbox-group)</h2>
+        {fruitOptions.map((v, i) => {
+          return (
+            <div key={i}>
+              <input
+                type="checkbox"
+                value={v}
+                checked={likeList.includes(v)}
+                onChange={(e) => {
+                  const targetValue = e.target.value;
+                  //先判斷是否有在state(likeList)中
+                  if (likeList.includes(targetValue)) {
+                    //如果有在state陣列中->從state陣列中移除
+                    //從陣列刪除這個值 = 回傳一個新陣列中不包含這個值
+                    const newLikeList = likeList.filter((v2, i2) => {
+                      return v2 !== targetValue;
+                    });
+                    setLikeList(newLikeList);
+                  } else {
+                    //如果沒有在state陣列中->加入到state陣列中
+                    const newLikeList = [...likeList, targetValue];
+                    setLikeList(newLikeList);
+                  }
+                }}
+              />
+              <label>{v}</label>
+            </div>
+          );
+        })}
       </section>
   </>
   )
