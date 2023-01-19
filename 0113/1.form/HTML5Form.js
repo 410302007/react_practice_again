0f26fcbb1fe02106c0b383646ddae2f18b0c852d
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-19 17:17:22 +0800

    可控表單元件: radio-group

--- 0113/1.form/HTML5Form.js
+++ 0113/1.form/HTML5Form.js
@@ -6,6 +6,11 @@ function HTML5Form(){
 
   //textarea
   const [textareaText, setTextareaText] = useState('');
+
+  //radio-group
+  //state中紀錄使用者選了單一值
+  const [food, setFood] = useState(''); //state中紀錄使用者選了哪個值
+  const foodOptions = ['排骨飯', '滷肉飯', '雞腿飯', '健康餐盒'];
   return (
   <>
   <h1>可控的表單元件範例</h1>
@@ -23,6 +28,24 @@ function HTML5Form(){
             setTextareaText(e.target.value);
           }}
         />
+      </section>
+      <section id="radio-group">
+        <h2>選項按鈕(radio-group)</h2>
+        {foodOptions.map((v, i) => {
+          return (
+            <div key={i}>
+              <input
+                type="radio"
+                value={v}
+                checked={food === v}
+                onChange={(e) => {
+                  setFood(e.target.value);
+                }}
+              />
+              <label>{v}</label>
+            </div>
+          );
+        })}
       </section>
   </>
   )
