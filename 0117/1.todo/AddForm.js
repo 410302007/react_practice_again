9bb5c065b24a3946ae5725162349e7330c9f8e16
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-31 23:57:04 +0800

    拆分AddForm-本地端狀態(local state)->讓文字輸入框變成可控表單元件

---
+++ 0117/1.todo/AddForm.js
@@ -0,0 +1,34 @@
+//在傳入參數時解構props
+//也可以這樣寫
+// function AddForm(props) {
+//   const {inputText, setInputText,addTodo} = props
+//  //...
+// }
+
+import { useState } from "react";
+
+function AddForm({addTodo}) {
+  //文字輸入框輸入用，可控表單元件用
+  //local state(本地端狀態)-->讓文字輸入框變成可控表單元件
+  const [inputText, setInputText] = useState('');
+  return (
+  <>
+     <input
+        type="text"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' && e.target.value.trim() !== '') {
+            //新增todo
+            addTodo(e.target.value)
+            //清空文字輸入框
+            setInputText('');
+          }
+        }}
+      />
+  </>
+  )
+}
+export default AddForm
